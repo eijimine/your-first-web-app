4dc0facea1ce71f4075a8009a6c25c90ede0ae13
eiji <eijimine@gmail.com>
2017-07-12 18:01:31 -0400

    server.rb complete

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
 end
@@ -8,6 +12,20 @@ get '/portfolio' do
   erb :gallery
 end
 
+get '/gallery' do
+  erb :portfolio
+end
+
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_link = ['www.google.ca',
+                'www.yahoo.ca',
+                'www.bing.com'
+              ]
+  erb :favourites
 end
